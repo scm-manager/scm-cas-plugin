603b7cf50e8f6951797fcc1309fb6cb7ab5f000a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-08-20 07:46:11 +0200

    Remove unnecessary lenient strictness

--- src/test/java/com/cloudogu/scm/cas/CasLoginLinkProviderTest.java
+++ src/test/java/com/cloudogu/scm/cas/CasLoginLinkProviderTest.java
@@ -29,14 +29,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 import static org.mockito.Answers.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 class CasLoginLinkProviderTest {
 
   @Mock(answer = RETURNS_DEEP_STUBS)
